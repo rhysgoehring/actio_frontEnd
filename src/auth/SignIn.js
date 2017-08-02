030d65c4c52620cc03aee9f0fd187601ffa49c84
spkelly <spkelly18@gmail.com>
2017-08-02 13:32:19 -0600

    added marketing section boilerplate to landing and signin

--- src/auth/SignIn.js
+++ src/auth/SignIn.js
@@ -73,6 +73,24 @@ class SignIn extends Component {
             </div>
           </article>
         </div>
+        <div className="marketing_sec_sign_in">
+          <div className="row">
+            <div className="container text-center">
+              <div className="col-md-4">
+                <h2>col 1</h2>
+                <hr />
+              </div>
+              <div className="col-md-4">
+                <h2>col 2</h2>
+                <hr />
+              </div>
+              <div className="col-md-4">
+                <h2>col 3</h2>
+                <hr />
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
