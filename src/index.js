3c5d503b4e223ef7b3f06941fcf32267a81e734b
rhysgoehring <rhysgoehring@gmail.com>
2017-08-11 15:36:37 -0600

    hiding

--- src/index.js
+++ src/index.js
@@ -31,6 +31,7 @@ const store = createStore(reducers, applyMiddleware(reduxThunk))
 window.GoogleMapsLoader = GoogleMapsLoader;
 GoogleMapsLoader.KEY = process.env.REACT_APP_MAP
 
+
 const token = localStorage.getItem('token');
 let currentUser = localStorage.getItem('currentUser')
 currentUser = JSON.parse(currentUser);
