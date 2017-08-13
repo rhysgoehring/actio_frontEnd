cbdaa005c01b808b8bbec21ecc72339db2c8d7d0
rhysgoehring <rhysgoehring@gmail.com>
2017-08-13 13:54:07 -0600

    removed font link, added font to public directory for faster loading

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import registerServiceWorker from './registerServiceWorker';
 import {Router, Route, IndexRoute, browserHistory} from 'react-router';
 import {createStore, applyMiddleware} from 'redux';
 import reduxThunk from 'redux-thunk';
-import logger from 'redux-logger';
+
 
 import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
@@ -26,10 +26,10 @@ import NewEvent from './components/NewEvent';
 import EditEvent from './components/EditEvent';
 import GoogleMapsLoader from 'google-maps';
 
-const middleware=[reduxThunk, logger]
 
 
-const store = createStore(reducers, applyMiddleware(...middleware))
+
+const store = createStore(reducers, applyMiddleware(reduxThunk))
 window.GoogleMapsLoader = GoogleMapsLoader;
 GoogleMapsLoader.KEY = process.env.REACT_APP_MAP
 
